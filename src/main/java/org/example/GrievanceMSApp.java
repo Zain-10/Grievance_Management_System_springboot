886bdd7698630036c2008db8d0c8486e6cb64b6d
Ahamed Zain <ahamedzainthappi10@gmail.com>
2024-10-03 19:26:39 +0530

    first commit

---
+++ src/main/java/org/example/GrievanceMSApp.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrievanceMS {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrievanceMS.class, args);
+    }
+
+}
